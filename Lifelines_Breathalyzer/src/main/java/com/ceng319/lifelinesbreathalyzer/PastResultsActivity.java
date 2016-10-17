b58b74dec8dae8756f701f093377f491ce77e302
Eugene Oliver <eugene_oliver@hotmail.com>
2016-10-16 21:21:04 -0400

    Added Results, Call and Past Results page. (Simple Layout)

--- Lifelines_Breathalyzer/src/main/java/com/ceng319/lifelinesbreathalyzer/PastResultsActivity.java
+++ Lifelines_Breathalyzer/src/main/java/com/ceng319/lifelinesbreathalyzer/PastResultsActivity.java
@@ -2,6 +2,8 @@ package com.ceng319.lifelinesbreathalyzer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class PastResultsActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class PastResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_past_results);
+
+        TextView textScrollable1 = (TextView)findViewById(R.id.PastBAClevels);
+        TextView textScrollable2 = (TextView)findViewById(R.id.PastBPMLevels);
+        //This is to enable scrolling on scrollview
+        textScrollable1.setMovementMethod(new ScrollingMovementMethod());
+        textScrollable2.setMovementMethod(new ScrollingMovementMethod());
     }
 }
