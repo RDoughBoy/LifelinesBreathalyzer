4c4b50caaed872d80d93c3db914682ac9e18af19
EugeneHasJeans <eugene_oliver@hotmail.com>
2016-11-15 23:12:39 -0500

    added taxi activity and runtime permissions

--- Lifelines_Breathalyzer/src/main/java/com/ceng319/lifelinesbreathalyzer/ResultsActivity.java
+++ Lifelines_Breathalyzer/src/main/java/com/ceng319/lifelinesbreathalyzer/ResultsActivity.java
@@ -1,7 +1,12 @@
 package com.ceng319.lifelinesbreathalyzer;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,8 +15,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class ResultsActivity extends AppCompatActivity {
+
+
+    private static final int REQUEST_CALL = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,7 @@ public class ResultsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Button button_call = (Button) findViewById(R.id.ResultsOption1);
+        Button button_taxi = (Button) findViewById(R.id.ResultsOption2);
         Button button_hotel = (Button) findViewById(R.id.ResultsOption3);
         Button button_past = (Button) findViewById(R.id.ResultsOption4);
 
@@ -28,7 +38,23 @@ public class ResultsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ResultsActivity.this, CallActivity.class);
-                startActivity(intent);
+                if (ContextCompat.checkSelfPermission(ResultsActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ResultsActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+                } else {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        button_taxi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResultsActivity.this, TaxiActivity.class);
+                if (ContextCompat.checkSelfPermission(ResultsActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ResultsActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+                } else {
+                    startActivity(intent);
+                }
             }
         });
 
@@ -74,5 +100,21 @@ public class ResultsActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_CALL:
+            {
+                if (grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), "Permission Granted!", Toast.LENGTH_SHORT).show();
+                    //Intent intent = new Intent(ResultsActivity.this, CallActivity.class);
+                    //startActivity(intent);
+                } else{
+                    Toast.makeText(getApplicationContext(), "Need Permission To Use The Phone!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
