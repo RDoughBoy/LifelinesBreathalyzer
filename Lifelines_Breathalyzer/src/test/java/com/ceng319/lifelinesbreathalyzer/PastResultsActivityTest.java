9f16c091bdea9bf7e72b8ba0cd8b097e6b3654eb
EugeneHasJeans <eugene_oliver@hotmail.com>
2016-12-12 16:25:26 -0500

    added 2 junit tests

---
+++ Lifelines_Breathalyzer/src/test/java/com/ceng319/lifelinesbreathalyzer/PastResultsActivityTest.java
@@ -0,0 +1,33 @@
+package com.ceng319.lifelinesbreathalyzer;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by eugen on 2016-12-12.
+ */
+public class PastResultsActivityTest {
+    private PastResultsActivity teststring;
+
+    @Before
+    public void setUp() throws Exception{
+        teststring = new PastResultsActivity();
+    }
+
+    @Test
+    public void convertArrayToString() throws Exception {
+        String[] myStringArray = {"this","is","a","test"};
+        String string="this\\r?\\nis\\r?\\na\\r?\\ntest";
+        assertEquals(string, teststring.convertArrayToString(myStringArray));
+    }
+
+    @Test
+    public void convertStringToArray() throws Exception {
+        String string="this\nis\na\ntest";
+        String[] stringArray = teststring.convertStringToArray(string);
+        assertEquals("this", stringArray[0]);
+    }
+
+}
